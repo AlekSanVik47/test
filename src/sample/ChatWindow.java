ba9b31462e0b5c6fb9cc90aaf1704fdaf10019b5
AlekSanVik47 <aleksandrvikbozhko@gmail.com>
2020-04-16 00:04:22 +0400

    Initial commit

---
+++ src/sample/ChatWindow.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javax.swing.*;
+
+public class ChatWindow {
+    private JTextArea textArea1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
